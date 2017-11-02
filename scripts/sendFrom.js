0d1cf1b44deea3cb2a75db05db3d8ca568234086
Andrii <dree0103@gmail.com>
2017-11-02 18:53:10 +0200

    give functionality

--- scripts/sendFrom.js
+++ scripts/sendFrom.js
@@ -17,7 +17,31 @@ let getInfo = () => {
     request.open("POST", "https://wristo-platform-backend-stg.herokuapp.com/api/v1/auth/sign_up", true);
     request.setRequestHeader("Content-type", "application/json");
     request.send(JSON.stringify(obj));
+    request.onreadystatechange = function () {
+        if(request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+            alert("welcome!");			
+        };
+    };
     
     
     console.log(JSON.stringify(obj))
+}
+
+let logIn = () => {
+    let email, password, doc, obj;
+    doc = document;
+    obj = {
+        email: doc.getElementById("email").value,
+        password: doc.getElementById("password").value
+    };
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://wristo-platform-backend-stg.herokuapp.com/api/v1/auth/sign_in", true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.send(JSON.stringify(obj));
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            alert("welcome!");			
+        }
+    };
+    console.log(JSON.stringify(obj));
 }
